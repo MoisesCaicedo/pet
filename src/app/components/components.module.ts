472849204001969b938bc0ec7623f8aa3bd27998
Mozzarth <moisescaicedo15@gmail.com>
2020-12-03 08:29:41 -0500

    ADD Componets and HeroComponent finished

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { OurServiceComponent } from './our-service/our-service.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { FooterComponent } from './footer/footer.component';
+import { CallToActionComponent } from './call-to-action/call-to-action.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [
+    NavBarComponent,
+    HeroComponent,
+    OurServiceComponent,
+    TestimonialsComponent,
+    HowItWorksComponent,
+    FooterComponent,
+    CallToActionComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [NavBarComponent]
+  exports: [
+    NavBarComponent,
+    HeroComponent,
+    OurServiceComponent,
+    TestimonialsComponent,
+    HowItWorksComponent,
+    FooterComponent,
+    CallToActionComponent]
 })
 export class ComponentsModule { }
---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+
+  @Input() title
+  @Input() descripcions
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
